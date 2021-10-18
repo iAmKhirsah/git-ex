f1e4cb04f21da62a7e836ce5b622c364ce3daa96
Ilya <khirsah2211@gmail.com>
2021-10-18 15:39:04 +0300

    before massive modal changes

--- js/proj-controller.js
+++ js/proj-controller.js
@@ -1,1 +1,27 @@
-'use strict'
+'use strict';
+
+renderProjects();
+
+function renderProjects() {
+  var projects = createProjects();
+  console.log(projects);
+  var strHTML = '';
+  for (var i = 1; i <= projects.length; i++) {
+    strHTML += `<div class="col-md-4 col-sm-6 portfolio-item">
+    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${i}">
+    <div class="portfolio-hover">
+    <div class="portfolio-hover-content">
+    <i class="fa fa-plus fa-3x"></i>
+    </div>
+    </div>
+    <img class="img-fluid" src="img/portfolio/${i}-portfolio.png" >
+    </a>
+    <div class="portfolio-caption">
+    <h4>${projects[i - 1].title}</h4>
+    <p class="text-muted">${projects[i - 1].name}</p>
+    </div>
+   </div>
+   `;
+  }
+  $('#portfolio').children('.container').children('.row').html(strHTML);
+}
--- js/service/proj-service.js
+++ js/service/proj-service.js
@@ -1,1 +1,34 @@
-'use strict'
+'use strict';
+
+function createProjects() {
+  var projs = [
+    {
+      id: 'pacman',
+      name: 'Pacman',
+      title: 'The Pacman',
+      desc: "It's pacman my guy",
+      url: 'https://iamkhirsah.github.io/pacman/',
+      publishedAt: new Date('02/10/2021'),
+      labels: ['pacman', 'css', 'js'],
+    },
+    {
+      id: 'minesweeper',
+      name: 'Minesweeper',
+      title: 'The Minesweeper',
+      desc: 'Become an expert at sea-mine disposal',
+      url: 'https://iamkhirsah.github.io/Minesweeper/',
+      publishedAt: new Date('6,10,2021'),
+      labels: ['sprint', 'css', 'js', 'bombs'],
+    },
+    {
+      id: 'bookshop',
+      name: 'Bookshop',
+      title: 'My bookshop',
+      desc: 'A bookshop',
+      url: 'https://iamkhirsah.github.io/bookshop/',
+      publishedAt: new Date('17/10/2021'),
+      labels: ['books', 'css', 'js', 'pretty'],
+    },
+  ];
+  return projs;
+}
